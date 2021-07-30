ec6b547aa2026cbf0ba8db04beeadeae137cb7d3
Fezeka Nzimande <60849112+fezeka-tech@users.noreply.github.com>
2021-07-30 14:17:22 +0100

    Update bmi.py
    rewrote code with slight changes

--- bmi.py
+++ bmi.py
@@ -1,14 +1,18 @@
 #This is a python program that calculates a person's BMI#
-#input person's height in centimetre and weight in kilograms#
-#output is weight divided by height in metres squared#
 
-print ("This person's BMI is")
 
-metre = cm * 0.01    #converts height in cm to m
-m_square = metre * metre
+def bmi():
+    def square_meter(cm):
+        meter = cm * 0.01  # converts height in cm to m
+        m_square = meter * meter
+        return m_square
 
-weight = int(input(65))
-height = int(input(1.8))
+    weight = int(input("Enter weight : "))
+    height = int(input("Enter height : "))
 
-bmi = weight/(height*height)
-print (bmi) 
+    body_mass = weight / square_meter(height)  
+    print(f'BMI is {body_mass:.2f}.')
+
+
+if __name__ == "__main__":
+    bmi()
