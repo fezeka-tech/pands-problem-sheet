c281a3b2d5bd36dbcfa609bcbdcb305c94234b7a
Fezeka Nzimande <60849112+fezeka-tech@users.noreply.github.com>
2021-07-30 15:30:31 +0100

    Update secondstring.py
    Corrected spacing and completed missing finishing code.

--- secondstring.py
+++ secondstring.py
@@ -3,12 +3,15 @@
 
 def second_string ( ) :
     sentence = input ("Please enter a sentence: ") .split ("The quick brown fox jumps over the lazy dog. ") #split sentence by space#
+    
     second_letter = [ ] #container to store every second letter#
 
     for words in sentence:
         second_letter.append(words[1::2]) #store every second letter in a word#
 
-strings = list (map(lambda x: x[::-1], second_letter)) #reverse second letter of a word#
+    strings = list (map(lambda x: x[::-1], second_letter)) #reverse second letter of a word#
 
-print ("The quick brown fox jumps over the lazy dog".join (strings[ ::-1])) #print all in reverse order#
+    print ("The quick brown fox jumps over the lazy dog".join (strings[ ::-1])) #print all in reverse order#
 
+if __name__ == '__main__':
+    second_string()
